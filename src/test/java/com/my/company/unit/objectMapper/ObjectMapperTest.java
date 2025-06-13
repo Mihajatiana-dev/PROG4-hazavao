4126d079c1e7d428831c20cd6ef4015fa9bda37a
Mihajatiana <hei.mihajatiana@gmail.com>
2025-06-13 09:41:16 +0300

    fix: fix: the test

--- src/test/java/com/my/company/unit/objectMapper/ObjectMapperTest.java
+++ src/test/java/com/my/company/unit/objectMapper/ObjectMapperTest.java
@@ -12,24 +12,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class ObjectMapperTest extends FacadeIT {
   @Autowired ObjectMapper injectedBean;
-  ObjectMapper newInstance = new ObjectMapper();
+
+  // Créez une nouvelle instance SANS le module Java Time pour le test
+  ObjectMapper newInstanceWithoutJavaTimeModule = new ObjectMapper();
 
   SomeClassWithDatetimeField someClassWithDatetimeField =
-      new SomeClassWithDatetimeField(Instant.now());
+          new SomeClassWithDatetimeField(Instant.now());
 
   @Test
   void new_instance_throws_on_java_datetime_module() {
     String jsonString = someClassWithDatetimeField.toJsonString();
     assertThrows(
-        InvalidDefinitionException.class,
-        () -> newInstance.readValue(jsonString, SomeClassWithDatetimeField.class));
+            InvalidDefinitionException.class,
+            () -> newInstanceWithoutJavaTimeModule.readValue(jsonString, SomeClassWithDatetimeField.class));
   }
 
   @Test
   void injected_bean_handles_java_datetime_module() {
     assertDoesNotThrow(
-        () ->
-            injectedBean.readValue(
-                someClassWithDatetimeField.toJsonString(), SomeClassWithDatetimeField.class));
+            () ->
+                    injectedBean.readValue(
+                            someClassWithDatetimeField.toJsonString(), SomeClassWithDatetimeField.class));
   }
 }
